2792d6b4410743a13d0b4f7904630a63514bf56d
Geeker01EN <39859848+Geeker01EN@users.noreply.github.com>
2018-06-08 13:01:35 +0200

    Initial commit

---
+++ Assets/AnimationController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rb;
+
+    [SerializeField] float speed;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            spriteRenderer.flipX = true;
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isIdle", false);
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            spriteRenderer.flipX = false;
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isIdle", false);
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        else
+        {
+            animator.SetBool("isIdle", true);
+            animator.SetBool("isRunning", false);
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+	}
+}
---
+++ Assets/BetterJump.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterJump : MonoBehaviour {
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    [SerializeField] BoxCollider2D[] boxCollider;
+
+    bool jumped;
+    bool grounded;
+
+    [SerializeField] float jumpSpeed;
+
+    [SerializeField] float fallMult;
+    [SerializeField] float ultraJumpMult;
+    [SerializeField] float risingMult;
+
+    void Awake () {
+        rb = GetComponent<Rigidbody2D>();
+        jumped = false;
+        grounded = true;
+        animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumped = true;
+            animator.SetBool("isJumping", true);
+        }
+	}
+
+    void FixedUpdate()
+    {
+        if (jumped && grounded)
+        {
+            grounded = false;
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+        else jumped = false;
+
+        if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = fallMult;
+        }
+        else if (rb.velocity.y > 0 && Input.GetButton("Jump"))
+        {
+            rb.gravityScale = ultraJumpMult;
+        }
+        else if (rb.velocity.y > 0)
+        {
+            rb.gravityScale = risingMult;
+        }
+        else
+        {
+            rb.gravityScale = 1;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D info)
+    {
+        if (info.otherCollider == boxCollider[1])
+        {
+            grounded = true;
+            animator.SetBool("isJumping", false);
+        }
+    }
+}
